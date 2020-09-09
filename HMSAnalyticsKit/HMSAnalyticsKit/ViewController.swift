6400912302a5145812d44f061b4a2f53682d17d8
Emre AYDIN <ea@ea.tc>
2020-09-09 16:43:26 +0300

    Project created..

---
+++ HMSAnalyticsKit/HMSAnalyticsKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HMSAnalyticsKit
+//
+//  Created by Emre AYDIN on 9/9/20.
+//  Copyright © 2020 Emre AYDIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
